f8c8ff767bc0a93de8ebdcd8f22ca2f71ac59ad5
Blake Yoder <yoderblake@gmail.com>
2016-05-12 16:52:57 -0400

    Added bg image in

--- game_window.rb
+++ game_window.rb
@@ -2,14 +2,15 @@ require 'gosu'
 require_relative 'character'
 
 class Game < Gosu::Window
-  SCREEN_HEIGHT = 1000
-  SCREEN_WIDTH = 1000
+  SCREEN_HEIGHT = 800
+  SCREEN_WIDTH = 1100
 
   def initialize
     super(SCREEN_WIDTH, SCREEN_HEIGHT, false)
-    self.caption = "Pokemon!"
+    self.caption = "Pokemon (WSP Edition)!"
     @character = Character.new
-    @background_image = Gosu::Image.new("images/bg-start.jpg", :tileable => true)
+    @background_image = Gosu::Image.new(self, "images/background.png", :tileable => false)
+    @large_font = Gosu::Font.new(self, "Early Gameboy", SCREEN_HEIGHT / 20)
   end
 
   #updates to game window
@@ -26,6 +27,11 @@ class Game < Gosu::Window
   def draw
     @character.draw
     @background_image.draw(0,0,0)
+    draw_text(SCREEN_WIDTH/4, SCREEN_HEIGHT/3, "Pokemon - WSP Edition", @large_font, 0xff000000)
+  end
+
+  def draw_text(x, y, text, font, color)
+    font.draw(text, x, y, 3, 1, 1, color)
   end
 
   def button_down(id)
