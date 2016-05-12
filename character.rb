a7b5bf1bcce454cbfab744f11205a232a6102306
Ryan Maas <ryan.h.maas@gmail.com>
2016-05-12 15:40:42 -0400

    Movement initialization

--- character.rb
+++ character.rb
@@ -1,27 +1,61 @@
 class Character
 
   #initialize and set x and y properties
-  def initialize
-    @image = Gosu::Image.new("images/8bitcharles.png")
-    @x = @y
-  end
-
-  #Change angle as desired
-  def turn_left
-    @angle -= 4.5
-  end
-
-  #Change angle as desired
-  def turn_right
-    @angle += 4.5
-  end
-
-  def move_char
+  def initialize(image, x, y)
+    @image = Gosu::Image.new(image)
+    @x      = x
+    @y      = y
+    @move_x = 0
+    @move_y = 0
+    @moving = false
   end
 
   def draw
     #this puts image at center of window
-    @image.draw_rot(1, 1, 1, 1)
+    @image.draw(@x, @y, 5)
   end
 
+  def update
+    if @moving then
+      if @move_x > 0 then
+        @move_x -= 1
+        @x += 1
+      elsif @move_x < 0 then
+        @move_x += 1
+        @x -= 1
+      elsif @move_x == 0 then
+        @moving = false
+      end
+
+      if @move_y > 0 then
+        @move_y -= 1
+        @y += 1
+      elsif @move_y < 0 then
+        @move_y += 1
+        @y -= 1
+      elsif @move_y == 0 then
+        @moving = false
+      end
+    end
+  end
+
+  def move_left
+    @move_x -= 50
+    @moving = true
+  end
+
+  def move_right
+    @move_x += 50
+    @moving = true
+  end
+
+  def move_up
+    @y -= 50
+    @moving = true
+  end
+
+  def move_down
+    @y += 50
+    @moving = true
+  end
 end
--- game_window.rb
+++ game_window.rb
@@ -8,19 +8,24 @@ class Game < Gosu::Window
   def initialize
     super(SCREEN_WIDTH, SCREEN_HEIGHT, false)
     self.caption = "Pokemon!"
-    @character = Character.new
+    @character = Character.new("images/8bitcharles.png", 50, 50)
     @background_image = Gosu::Image.new("images/bg-start.jpg", :tileable => true)
   end
 
   #updates to game window
   def update
     if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft then
-      @character.turn_left
+      @character.move_left
     end
     if Gosu::button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight then
-      @character.turn_right
+      @character.move_right
     end
-    @character.move_char
+    if Gosu::button_down? Gosu::KbUp or Gosu::button_down? Gosu::GpUp then
+      @character.move_up
+    end
+    if Gosu::button_down? Gosu::KbDown or Gosu::button_down? Gosu::GpDown then
+      @character.move_down
+    end
   end
 
   def draw
@@ -29,10 +34,22 @@ class Game < Gosu::Window
   end
 
   def button_down(id)
-    if id == Gosu::KbEscape
+    case id
+    when Gosu::KbUp
+      puts "Up button_down"
+    when Gosu::KbDown
+      puts "Down button_down"
+    when Gosu::KbLeft
+      @character.move_left
+      puts "LEFT"
+    when Gosu::KbRight
+      @character.move_right
+      puts "RIGHT"
+    when Gosu::KbEscape
       close
     end
   end
+
 
 end
 
