6d022f3028ac94a0abb54b6d0e86918681901be1
Blake Yoder <yoderblake@gmail.com>
2016-05-12 22:46:01 -0400

    Changes on game window

--- game_window.rb
+++ game_window.rb
@@ -6,7 +6,7 @@ class Game < Gosu::Window
   # lol constants
   SCREEN_HEIGHT = 800
   SCREEN_WIDTH = 1100
-  GAME_LIMIT = 3
+  GAME_LIMIT = 0
   RESTRICTED_X_LEFT = -13
   RESTRICTED_X_RIGHT = 1047
   RESTRICTED_Y_TOP = 0
@@ -26,9 +26,9 @@ class Game < Gosu::Window
     @chars = Character.descendants.sample(5)
     @chars.each do |char|
       @char = char.new
-      @char.create(@char.get_sprite, generateRandomXCoord, generateRandomYCoord)
+      @background_image.insert(@char.get_sprite, generateRandomXCoord, generateRandomYCoord)
     end
-    @large_font = Gosu::Font.new(self, "Early Gameboy", SCREEN_HEIGHT / 20)
+    @large_font = Gosu::Font.new(self, "Early Gameboy", SCREEN_HEIGHT / 10)
   end
 
   #updates to game window
